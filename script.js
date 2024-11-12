054bc132b9e1948e075ba1709a5a412efe30e4bf
Szántó Benjámin <szantobenjamin97@gmail.com>
2024-11-12 20:26:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ hatter.addEventListener('click', async function() {
     if (zarva === true) {
         for (let index = 0; index <= 10; index++) {
            kep.src = images[index].src; // Már előre cache-elt képet használunk
-           await new Promise(pr => setTimeout(pr, 13)); // Késleltetési idő
+           await new Promise(pr => setInterval(pr, 13)); // Késleltetési idő
         }
         const randomIndex = Math.floor(Math.random() * uzik.length); // [0,5]
         szoveg.textContent = uzik[randomIndex];
